b6c3e18f3dddd7a512fd367a1f8e83c46feca91c
Mrdadgoku <dr084891@gmail.com>
2024-05-31 08:29:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b3d0e737737d67a5bf5a5.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/1481a6df3ed3ecfc21cfa.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/1481a6df3ed3ecfc21cfa.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sastatony")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/team_ligit")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BEST_FRIEND_FOREVER1230")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Goku_Modz")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
